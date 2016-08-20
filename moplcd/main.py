53d13c0cd86257e22a325746134181bc01ef09f9
H4genT <braas.hagen@gmail.com>
2016-08-20 23:16:16 +0200

    Create main.py

---
+++ moplcd/main.py
@@ -0,0 +1,15 @@
+from mopidy import core
+import pykka
+from screen  import lines
+
+class moplcdFrontend(pykka.ThreadingActor, core.CoreListener):
+        def __init__(self, config, core):
+                super(Output, self).__init__()
+                self.core = core
+                self.lines = lines()
+
+        def mute_changed(mute):
+                lines.write("mute")
+
+        def playback_state_changed(old_state, new_state):
+                lines.clear("playback")
