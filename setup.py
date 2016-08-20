cf443f77a9fbaecf4128e5db473d880a32bceecc
H4genT <braas.hagen@gmail.com>
2016-08-20 23:00:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, unicode_literals
+
+import re
+from setuptools import setup, find_packages
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='moplcd',
+    version=get_version('mopidy_soundspot/__init__.py'),
+    url='https://github.com/H4genT/lcd.git',
+    license='private',
+    author='H4genT',
+    author_email='braas.hagen@gmail.com',
+    description='Very short description',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 0.14',
+        'Pykka >= 1.1',
+        'pysoundspot',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'moplcd = mopidy_lcd:Extension',
+        ],
+    },
+    classifiers=[
+        
+    ],
+)
