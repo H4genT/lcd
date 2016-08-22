d554b090a3f62ade1b197615d9664395d8921b31
H4genT <braas.hagen@gmail.com>
2016-08-22 21:05:36 +0200

    Update __init__.py

--- moplcd/__init__.py
+++ moplcd/__init__.py
@@ -22,26 +22,10 @@ class Extension(ext.Extension):
         conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
         return config.read(conf_file)
 
-    def get_config_schema(self):
-        schema = super(Extension, self).get_config_schema()
-        schema['username'] = config.String()
-        schema['password'] = config.Secret()
-        return schema
-
-    def get_command(self):
-        from .commands import moplcdCommand
-        return moplcdCommand()
-
-    def validate_environment(self):
-        # Any manual checks of the environment to fail early.
-        # Dependencies described by setup.py are checked by Mopidy, so you
-        # should not check their presence here.
-        pass
-
     def setup(self, registry):
         # You will typically only do one of the following things in a
         # single extension.
 
          # Register a frontend
-        from .frontend import moplcdFrontend
+        from .actor import moplcdFrontend
         registry.add('frontend', moplcdFrontend)
