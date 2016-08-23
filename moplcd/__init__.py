a1d3fecfb721d534e14f4d70d62828507dd643f4
H4genT <braas.hagen@gmail.com>
2016-08-23 20:15:00 +0200

    Update __init__.py

--- moplcd/__init__.py
+++ moplcd/__init__.py
@@ -8,9 +8,6 @@ from mopidy import config, exceptions, ext
 
 __version__ = '0.1'
 
-# If you need to log, use loggers named after the current Python module
-logger = logging.getLogger(__name__)
-
 
 class Extension(ext.Extension):
 
