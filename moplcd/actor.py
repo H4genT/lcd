867834a2911de7e25a8ea7a41735e54ad251c500
H4genT <braas.hagen@gmail.com>
2016-08-24 19:37:09 +0200

    Update actor.py

--- moplcd/actor.py
+++ moplcd/actor.py
@@ -6,11 +6,11 @@ class moplcdFrontend(pykka.ThreadingActor, core.CoreListener):
         def __init__(self, config, core):
                 super(moplcdFrontend, self).__init__()
                 self.core = core
-                self.lines = lines()
+                lines = lines()
                 self.lines.write("Actor initialisiert")
 
         def mute_changed(self, mute):
-                self.lines.write("mute")
+                lines.write("mute")
 
         def playback_state_changed(old_state, new_state):
-                self.lines.write("new_state")
+                lines.write("new_state")
