dac9ca1ca0738ff8c1a83dfd9ab0c591d26618b0
H4genT <braas.hagen@gmail.com>
2016-08-25 10:44:46 +0200

    Update actor.py

--- moplcd/actor.py
+++ moplcd/actor.py
@@ -8,30 +8,17 @@ class moplcdFrontend(pykka.ThreadingActor, core.CoreListener):
         def __init__(self, config, core):
                 super(moplcdFrontend, self).__init__()
                 self.core = core
-                stderr = sys.stderr.write;
-                self.lcd = Lcd()
-                # Try to trap any exit errors and cleanup GPIO
-                def exit_fn():
-                        if not traceback.format_exc().startswith('None'):
-                                s=traceback.format_exc()
-                # Register
-                #atexit.register(exit_fn)
-                def interrupt():
-                        return False
-                boardrevision = 2
-                self.lcd.init(boardrevision)
-                self.lcd.setWidth(20)
-
+                
 #        def mute_changed(self, mute):
 #                self.lines.write("mute")
                 
         def track_playback_paused(self, tl_track, time_position):
-                self.lcd.line3("Paused -" + tl_track)
-                ShowTime()
+                self.lines.track(False, tl_track)
+                self.lines.time
         
         def track_playback_resumed(self, tl_track, time_position):
-                self.lcd.line3(tl_track)
-                ShowTime()
+                self.lines.track(True, tl_track)
+                self.lines.time
                 
         def ShowTime(self):
                 self.lcd.line1(time.strftime(" %d.%m.%Y - %H:%M"))
