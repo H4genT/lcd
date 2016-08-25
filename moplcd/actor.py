2a0bc392f10dadccf870ed3f7739bef3b22a9da1
H4genT <braas.hagen@gmail.com>
2016-08-25 08:23:11 +0200

    Update actor.py

--- moplcd/actor.py
+++ moplcd/actor.py
@@ -1,21 +1,37 @@
+import pykka, time
 from mopidy import core
-import pykka
 from screen  import lines
+from lcd_class import Lcd
+
 
 class moplcdFrontend(pykka.ThreadingActor, core.CoreListener):
         def __init__(self, config, core):
                 super(moplcdFrontend, self).__init__()
                 self.core = core
-                self.lines = lines()
-                self.lines.write("Actor initialisiert")
+                stderr = sys.stderr.write;
+                self.lcd = Lcd()
+                # Try to trap any exit errors and cleanup GPIO
+                def exit_fn():
+                        if not traceback.format_exc().startswith('None'):
+                                s=traceback.format_exc()
+                # Register
+                #atexit.register(exit_fn)
+                def interrupt():
+                        return False
+                boardrevision = 2
+                self.lcd.init(boardrevision)
+                self.lcd.setWidth(20)
 
-        def mute_changed(self, mute):
-                self.lines.write("mute")
-                
+#        def mute_changed(self, mute):
+#                self.lines.write("mute")
                 
         def track_playback_paused(self, tl_track, time_position):
-                self.lines.write("Paused")
+                self.lcd.line3("Paused -" tl_track)
+                ShowTime()
         
         def track_playback_resumed(self, tl_track, time_position):
-                self.lines.write("Resumed")
+                self.lcd.line3(tl_track)
+                ShowTime()
                 
+        def ShowTime(self):
+                self.lcd.line1(time.strftime(" %d.%m.%Y - %H:%M"))
