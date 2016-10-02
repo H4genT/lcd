2d01267f036e37647b2c6e7fee94e9e44d64d065
H4genT <braas.hagen@gmail.com>
2016-10-02 19:13:37 +0200

    Update actor.py

--- moplcd/actor.py
+++ moplcd/actor.py
@@ -12,18 +12,22 @@ class moplcdFrontend(pykka.ThreadingActor, CoreListener):
                 self.lines = lines()
                 
         def on_start(self):
+                
                 #self.lines.startstop(True)
         
         def on_stop(self):
+                
                 #self.lines.startstop(False)
 
 #        def mute_changed(self, mute):
 #                self.lines.write("mute")
                 
         def track_playback_paused(self, tl_track, time_position):
+                
                 #self.lines.track(False, "Pause")
                 #self.lines.time()
         
         def track_playback_resumed(self, tl_track, time_position):
+                
                 #self.lines.track(True, "Play")
                 #self.lines.time()
