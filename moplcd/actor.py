df5fa62570320d00716a59f0097b5fb4e81f2898
H4genT <braas.hagen@gmail.com>
2016-10-02 16:52:27 +0200

    Update actor.py

--- moplcd/actor.py
+++ moplcd/actor.py
@@ -10,17 +10,14 @@ class moplcdFrontend(pykka.ThreadingActor, CoreListener):
                 self.core = core
                 self.config = config
                 self.lines = lines()
-                print("Actor Init")
                 
         def on_start(self):
                 logger.debug('moplcd started')
                 #self.lines.startstop(True)
-                print("Actor Start")
         
         def on_stop(self):
                 logger.debug('moplcd stopped')
                 #self.lines.startstop(False)
-                print("Actor Stop")
 
 #        def mute_changed(self, mute):
 #                self.lines.write("mute")
@@ -28,13 +25,10 @@ class moplcdFrontend(pykka.ThreadingActor, CoreListener):
         def track_playback_paused(self, tl_track, time_position):
                 #self.lines.track(False, "Pause")
                 #self.lines.time()
-                print("Actor Paused")
         
         def track_playback_resumed(self, tl_track, time_position):
                 #self.lines.track(True, "Play")
                 #self.lines.time()
-                print("Actor Resumed")
                 
         def on_event(self, event, **kwargs):
-                print("Actor Event")
-                return CoreListener.on_event(self, event, **kwargs)
+                #return CoreListener.on_event(self, event, **kwargs)
