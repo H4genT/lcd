a016c926fbbde35772f806c998e01665e37f431d
H4genT <braas.hagen@gmail.com>
2017-03-19 11:31:40 +0100

    a

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     entry_points={
         'mopidy.ext': [
-            'moplcd = moplcd:Extension',
+            'moplcd = mopidy_moplcd:Extension',
         ],
     },
     classifiers=[
