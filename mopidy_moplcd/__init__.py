91b1e781ec6f7c65ea49bd756aa08c155e044c80
H4genT <braas.hagen@gmail.com>
2017-03-19 12:20:14 +0100

    indent

--- mopidy_moplcd/__init__.py
+++ mopidy_moplcd/__init__.py
@@ -10,17 +10,15 @@ __version__ = '0.1.1'
 
 
 class Extension(ext.Extension):
-
+	
     dist_name = 'moplcd'
     ext_name = 'moplcd'
     version = __version__
-
+	
     def get_default_config(self):
 		conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
         return config.read(conf_file)
-        
-    
-
+		
     def setup(self, registry):
 		from .actor import moplcdFrontend
 		registry.add('frontend', moplcdFrontend)
